a3414a9f9c7eb82d969d11fc2e99b8a6a13aea65
hal9000s <informatics.assignment@gmail.com>
2025-08-25 09:52:36 +0700

    add get ip func

--- src/app.constant.js
+++ src/app.constant.js
@@ -29,7 +29,7 @@ exports.DOCUMENT_ALLOWED_EXTENSIONS = [
 exports.ALLOWED_HOST = [
     `${getIp()}:${process.env.NODE_PORT}`,
     'eklaim.rsudmajalengka.co.id',
-    'eklaim.rsudmajalengka.my.id'
+    'eklaim.rsmajalengka.my.id'
 ];
 exports.USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
